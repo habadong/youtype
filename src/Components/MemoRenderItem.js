bb9dc3da21209a1278160708fffb2e599de63e19
habadong <44386449+habadong@users.noreply.github.com>
2022-03-18 17:50:56 +0900

    firebase cloud storage 추가 & MemoScreen 수정

--- src/Components/MemoRenderItem.js
+++ src/Components/MemoRenderItem.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { View, Text, StyleSheet, Pressable } from 'react-native'
+import { View, Text, StyleSheet, Pressable, Image } from 'react-native'
 import { widthPercentage, heightPercentage, fontPercentage } from '../Functions/ResponsiveSize'
 
-export default function MemoRenderItem({ itemId, title, content, navigation }) {
+export default function MemoRenderItem({ itemId, title, content, img, navigation }) {
     return (
         <Pressable 
             style={styles.memoCard}
@@ -11,8 +11,16 @@ export default function MemoRenderItem({ itemId, title, content, navigation }) {
                     itemId: itemId
                 })
             }}>
-            <Text style={styles.title}>{title}</Text>
-            <Text style={styles.content}>{content}</Text>
+            <View>
+                <Text style={styles.title}>{title}</Text>
+                <Text style={styles.content}>{content}</Text>
+            </View>
+            <View>
+                <Image 
+                    style={styles.img} 
+                    source={{uri: img}} 
+                    defaultSource={{uri: 'https://cms-assets.tutsplus.com/uploads/users/1199/posts/30670/preview_image/react.jpg'}} />
+            </View>
         </Pressable>
     )
 }
@@ -20,16 +28,28 @@ export default function MemoRenderItem({ itemId, title, content, navigation }) {
 const styles = StyleSheet.create({
     memoCard: {
       flex: 1,
+      flexDirection: 'row',
       backgroundColor: 'white',
       borderRadius: 12,
       marginBottom: heightPercentage(0.2),
       paddingHorizontal: widthPercentage(0.3),
       paddingVertical: heightPercentage(0.1),
     },
+    memoCardLeft: {
+        flex: 3
+
+    },
     title: {
         fontSize: fontPercentage(15)
     },
     content: {
         fontSize: fontPercentage(10)
+    },
+    memoCardRight: {
+        flex: 1
+    },
+    img: {
+        width: widthPercentage(0.5),
+        height: heightPercentage(0.5),
     }
 });
--- src/MemoTap/MemoScreen.js
+++ src/MemoTap/MemoScreen.js
@@ -1,35 +1,55 @@
 import React from 'react'
-import { View, Text, StyleSheet, SafeAreaView, FlatList } from 'react-native'
+import { View, Text, StyleSheet, SafeAreaView, FlatList, Button } from 'react-native'
 import MemoRenderItem from '../Components/MemoRenderItem'
 import MemoListHeader from '../Components/MemoListHeader'
 import { widthPercentage, heightPercentage, fontPercentage } from '../Functions/ResponsiveSize'
+import firestore from '@react-native-firebase/firestore';
 
 const DATA = [
   {
     id: 1,
     title: 'first memo',
-    content: 'this is first memo card'
+    content: 'this is first memo card',
+    img: 'https://img.piku.co.kr/w/uploads/5u2qx2/003ae4c2caa72e35c31d6bf6f57290f8.jpg'
   },
   {
     id: 2,
     title: 'second memo',
-    content: 'this is second memo card'
+    content: 'this is second memo card',
+    img: 'https://img.piku.co.kr/w/uploads/5u2qx2/94abd2654f8b9b23a65f08b15c39b821.jpg'
   },
   {
     id: 3,
     title: 'third memo',
-    content: 'this is second memo card'
+    content: 'this is third memo card',
+    img: 'https://img.piku.co.kr/w/uploads/5u2qx2/37d56bdaa52e1bb78896052f93396727.jpg'
+  },
+  {
+    id: 4,
+    title: '4th memo',
+    content: 'this is 4th memo card',
   },
 ]
 
 
 export default function MemoScreen({ navigation }) {
 
+  // const ref = firestore().collection('users')
+
+
+  // async function addData() {
+  //   await ref.add({
+  //     title: 'hi',
+  //     check: false,
+  //   })
+  // }
+
   const memoRender = ({ item }) => (
     <MemoRenderItem 
       itemId={item.id} 
       title={item.title} 
       content={item.content} 
+      img={item.img}
       navigation={navigation}
     />
   )
